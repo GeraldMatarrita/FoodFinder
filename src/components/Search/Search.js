122ffad1177d226b2b59063c8d57e51b2e646618
Gerald Matarrita <68918784+GeraldMatarrita@users.noreply.github.com>
2025-01-23 04:42:18 -0600

    Update Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -64,7 +64,7 @@ export const Search = () => {
             alt="Administrator"
             className="icon"
           />
-          <button className="btn btn-dark ms-4 btn-lg" onClick={handleLogout}>
+          <button className="btn btn-dark ms-4 btn-lg" id="cerrarSesion" onClick={handleLogout}>
             Cerrar Sesión
           </button>
         </div>
