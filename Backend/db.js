8f7952c1dd53594f0172c45afd6170a9089ccf64
Gerald Matarrita <68918784+GeraldMatarrita@users.noreply.github.com>
2025-02-15 14:18:11 -0600

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'admin',
-    host: 'dpg-cu83m2i3esus73aoufsg-a.oregon-postgres.render.com',
-    database: 'foodfinder_2cfd',
-    password: '4Gpjl73DbSfJleEp22site6jVXQY01Vn',
-    port: 5432,
+    user: '',
+    host: '',
+    database: '',
+    password: '',
+    port: ,
     ssl: { rejectUnauthorized: false } // Necesario para Render
 });
 
